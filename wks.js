b33634078fa4c6a9674f50d55f460f78f8ac07af
tszh4922 <tszh4922@gmail.com>
2026-03-24 07:32:56 +0800

    Create wks.js

---
+++ wks.js
@@ -0,0 +1,23 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    
+    // 【修改重点】这里填你 Pages 的原始域名 (xxx.pages.dev)
+    const targetHost = 'your-project.pages.dev'; 
+    
+    // 修改请求的 Host，确保 Pages 能够识别
+    const newRequest = new Request(request);
+    newRequest.headers.set('Host', targetHost);
+    
+    // 构造新的请求 URL
+    const targetUrl = `https://${targetHost}${url.pathname}${url.search}`;
+    
+    // 加上 redirect: 'follow' 确保处理可能的 301 跳转
+    return fetch(targetUrl, {
+      method: newRequest.method,
+      headers: newRequest.headers,
+      body: newRequest.body,
+      redirect: 'follow'
+    });
+  }
+};
