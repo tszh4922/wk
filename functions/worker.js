6f2504a6d7d372e1bdda197df01708df1e89534d
tszh4922 <tszh4922@gmail.com>
2026-04-06 15:11:03 +0800

    Update worker.js

--- functions/worker.js
+++ functions/worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.hostname = 'tszh.us.ci'; 
+    url.hostname = 'yx.tszh.us.ci'; 
     return fetch(new Request(url, request));
   }
 };
